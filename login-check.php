d37722d87959b3e0a6017f17d9d4c3757f386dbe
RizaruKecoak <55182873+RizaruKecoak@users.noreply.github.com>
2019-10-16 18:04:19 +0700

    Sign Up and Login

---
+++ login-check.php
@@ -0,0 +1,19 @@
+<?php
+include 'conn.php';
+
+
+	session_start();
+	$email = $_POST['email'];
+	$password = $_POST['pass'];
+
+	$data = mysqli_query($conn,"SELECT * FROM login WHERE email='$email' and password='$password'");
+	$cek = mysqli_num_rows($data);
+
+	if($cek > 0){
+		$_SESSION['email'] = $email;
+        $_SESSION['status'] = "login";
+        header('location:asjdsa.html');
+	} else {
+		$msg='Gagal Login';
+	}
+?>
