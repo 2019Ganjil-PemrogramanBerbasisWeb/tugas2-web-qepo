a2fed48856e4127442c070420424f19f5def82e0
RizaruKecoak <55182873+RizaruKecoak@users.noreply.github.com>
2019-10-21 14:26:09 +0700

    Update login-check.php

--- login-check.php
+++ login-check.php
@@ -1,10 +1,24 @@
-<?php 
+<?php
+
+
 include 'conn.php';
- 
+
 $username = $_POST['email'];
 $password = $_POST['password'];
- 
-$query = mysql_query("select * from login where email='$username' and password='$password'");
-$cek = mysql_num_rows($query);
-echo $cek;
+
+
+$data = mysqli_query($link, " SELECT * FROM users WHERE email='$email' AND password='$password'");
+
+$cek = mysqli_num_rows($data);
+if($cek > 0)
+{
+  session_start();
+  $_SESSION['email'] = $email;
+  $_SESSION['status'] = "Login";
+  setcookie("mesage", "delete", time()-1); //hapus pesan setcookie
+  header("location:index.php");
+}
+else {
+  echo '<p>Data yang anda masukkan salah</p><br><a href="login.php">Kembali</a>'; 
+}
 ?>
