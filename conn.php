936940dce04d002459de29b358a0562e5378d318
Rinnabel <55181896+Rinnabel@users.noreply.github.com>
2019-10-22 19:51:44 +0700

    Add files via upload

--- conn.php
+++ conn.php
@@ -2,13 +2,14 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$database = "databes";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "";
+echo "Connected successfully";
 ?>
